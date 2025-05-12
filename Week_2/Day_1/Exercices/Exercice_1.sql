acfd0e3662a118a1397105696a756536e23ad933
Salmane1991 <el.hamdouni.salmane@gmail.com>
2025-05-12 15:48:25 +0100

    error sql

--- Week_2/Day_1/Exercices/Exercice_1.sql
+++ Week_2/Day_1/Exercices/Exercice_1.sql
@@ -29,7 +29,7 @@ SELECT * FROM items WHERE price>80;
 SELECT * FROM items WHERE price<300;
 SELECT * FROM customers WHERE last_name='Smith';
 SELECT * FROM customers WHERE last_name='Jones';
-SELECT * FROM customers WHERE last_name !='Jones';
+SELECT * FROM customers WHERE last_name !='Scott';
 
 
 
