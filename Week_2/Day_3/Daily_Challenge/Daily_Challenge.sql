f6a7d2daf973eaa620389ceb6692e381d35d0b0f
Salmane1991 <el.hamdouni.salmane@gmail.com>
2025-05-14 23:22:49 +0100

    day3 full commit

---
+++ Week_2/Day_3/Daily_Challenge/Daily_Challenge.sql
@@ -0,0 +1,78 @@
+CREATE TABLE Customer(
+id SERIAL PRIMARY KEY ,
+first_name VARCHAR (20) NOT NULL,
+last_name VARCHAR (20) NOT NULL
+
+);
+CREATE TABLE customer_profile (
+  id SERIAL PRIMARY KEY,
+  isLoggedIn BOOLEAN DEFAULT FALSE,
+  customer_id INTEGER UNIQUE REFERENCES customer(id)
+);
+ INSERT INTO Customer (first_name, last_name) 
+ VALUES 
+  ('John', 'Doe'),
+  ('Jerome', 'Lalu'),
+  ('Lea', 'Rive');
+INSERT INTO customer_profile (isLoggedIn, customer_id)
+VALUES 
+(TRUE, (SELECT id FROM Customer WHERE first_name = 'John' AND last_name = 'Doe')),
+(DEFAULT, (SELECT id FROM Customer WHERE first_name = 'Jerome' AND last_name = 'Lalu')); 
+SELECT first_name
+FROM Customer
+INNER JOIN customer_profile ON customer.id=customer_profile.customer_id WHERE customer_profile.isLoggedIn=True
+SELECT customer.first_name , customer_profile.isLoggedIn
+FROM customer
+LEFT JOIN customer_profile ON customer.id=customer_profile.customer_id
+SELECT COUNT(*) FROM  customer 
+RIGHT JOIN customer_profile ON customer.id=customer_profile.customer_id
+WHERE customer_profile.isLoggedIn=FALSE
+
+--Part2
+CREATE TABLE Book(
+book_id SERIAL PRIMARY KEY,
+title VARCHAR(100) NOT NULL,
+author VARCHAR(100) NOT NULL
+);
+INSERT INTO Book(title, author)
+VALUES
+('Alice In Wonderland', 'Lewis Carroll'),
+('Harry Potter', 'J.K Rowling'),
+('To kill a mockingbird', 'Harper Lee');
+CREATE TABLE Student(
+student_id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL UNIQUE,
+age INTEGER CHECK (age <= 15)
+);
+INSERT INTO Student(name, age)
+VALUES
+('John', 12),
+('Lera', 11),
+('Patrick', 10),
+('Bob', 14);
+CREATE TABLE Library(
+book_fk_id INTEGER REFERENCES Book(book_id) ON DELETE CASCADE ON UPDATE CASCADE,
+student_fk_id INTEGER REFERENCES Student(student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+borrowed_date DATE,
+PRIMARY KEY (book_fk_id, student_fk_id)
+);
+INSERT INTO Library(book_fk_id, student_fk_id, borrowed_date)
+VALUES
+((SELECT book_id FROM Book WHERE title = 'Alice In Wonderland'), (SELECT student_id FROM Student WHERE name = 'John'), '2022-02-15'),
+((SELECT book_id FROM Book WHERE title = 'To kill a mockingbird'), (SELECT student_id FROM Student WHERE name = 'Bob'), '2021-03-03'),
+((SELECT book_id FROM Book WHERE title = 'Alice In Wonderland'), (SELECT student_id FROM Student WHERE name = 'Lera'), '2021-05-23'),
+((SELECT book_id FROM Book WHERE title = 'Harry Potter'), (SELECT student_id FROM Student WHERE name = 'Bob'), '2021-08-12');
+SELECT * FROM Library;
+SELECT Student.name, Book.title
+FROM Library
+JOIN Student ON Library.student_fk_id = Student.student_id
+JOIN Book ON Library.book_fk_id = Book.book_id;
+SELECT AVG(age)
+FROM Student
+WHERE student_id IN (
+SELECT student_fk_id FROM Library
+WHERE book_fk_id = (SELECT book_id FROM Book WHERE title = 'Alice In Wonderland')
+);
+DELETE FROM Student WHERE name = 'Bob';
+
+ 
