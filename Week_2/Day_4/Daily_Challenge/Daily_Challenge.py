949de39cba8266f0c82a38c7dcb8d2501a420f01
Salmane1991 <el.hamdouni.salmane@gmail.com>
2025-05-16 23:54:34 +0100

    dc day4

---
+++ Week_2/Day_4/Daily_Challenge/Daily_Challenge.py
@@ -0,0 +1,40 @@
+import requests
+import random
+import psycopg2
+
+DB_NAME = "Countries"
+USER = "postgres"
+PASSWORD = "0000"
+HOST = "localhost"
+PORT = "5432"
+
+url = "https://restcountries.com/v3.1/all"
+response = requests.get(url)
+data = response.json()
+
+random_countries = random.sample(data, 10)
+
+conn = psycopg2.connect(
+    dbname=DB_NAME,
+    user=USER,
+    password=PASSWORD,
+    host=HOST,
+    port=PORT
+)
+cur = conn.cursor()
+
+for country in random_countries:
+    name = country['name']['common']
+    capital = country.get('capital', ['Unknown'])[0]
+    flag = country['flags']['png']
+    subregion = country.get('subregion', 'Unknown')
+    population = country.get('population', 0)
+
+    cur.execute("""
+        INSERT INTO countries (name, capital, flag, subregion, population)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (name, capital, flag, subregion, population))
+
+conn.commit()
+cur.close()
+conn.close()
