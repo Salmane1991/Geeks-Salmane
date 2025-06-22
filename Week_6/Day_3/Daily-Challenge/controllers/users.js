e292e41cd76bdb87ace5c69ed2f6e853bd661767
Salmane1991 <el.hamdouni.salmane@gmail.com>
2025-06-22 20:40:23 +0100

    dc d3w6

--- Week_6/Day_3/Daily-Challenge/controllers/users.js
+++ Week_6/Day_3/Daily-Challenge/controllers/users.js
@@ -1,5 +1,5 @@
 const db = require("../models/db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
   const { username, pass, email, first_name, last_name } = req.body;
@@ -22,6 +22,84 @@ const register = async (req, res) => {
 
     res.send({ message: "User registered successfully" });
   } catch (err) {
-    res.send({ message: "Registration failed" });
+    console.error("Error during registration:", err);
+    res
+      .status(500)
+      .send({ message: "Registration failed", error: err.message });
   }
 };
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await db("hashpwd").where({ username }).first();
+
+    if (!user) {
+      return res.status(401).send("User Not Found!");
+    }
+
+    const userPw = user.password;
+    const isValid = bcrypt.compareSync(password, userPw);
+
+    if (isValid) {
+      return res.send("Password is correct. You are logged In");
+    } else {
+      return res.send("Password is incorrect. Try Again!");
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ message: "Login failed due to server error" });
+  }
+};
+
+const users = async (req, res) => {
+  try {
+    const allUsers = await db("users").select("*");
+    if (allUsers.length > 0) {
+      return res.send(allUsers);
+    }
+  } catch (err) {
+    return res.status(404).send("List not Found!");
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const searchID = req.params.id;
+    const userId = await db("users").where({ id: searchID }).first();
+    if (userId) {
+      return res.json(userId);
+    } else {
+      return res.send("User not found");
+    }
+  } catch (err) {
+    return res.send("Error Message : " + err);
+  }
+};
+
+const updateUserById = async (req, res) => {
+  try {
+    const searchID = req.params.id;
+    const userId = await db("users").where({ id: searchID }).first();
+
+    if (!userId) {
+      return res.send("User not found");
+    }
+
+    for (const key in req.body) {
+      if (key in userId && typeof req.body[key] === typeof userId[key]) {
+        await db("users")
+          .where({ id: searchID })
+          .update({ [key]: req.body[key] });
+      }
+    }
+
+    res.send("User is updated");
+  } catch (err) {
+    return res.send("Error Message: " + err);
+  }
+};
+
+module.exports = { register, login, users, getUserById, updateUserById };
--- Week_6/Day_3/Daily-Challenge/routes/users.js
+++ Week_6/Day_3/Daily-Challenge/routes/users.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
-
+const {
+  register,
+  login,
+  users,
+  getUserById,
+  updateUserById,
+} = require("../controllers/users");
 
 router.post("/register", register);
+router.post("/login", login);
+router.get("/users", users);
+router.get("/users/:id", getUserById);
+router.put("/users/:id", updateUserById);
+
+module.exports = router;
